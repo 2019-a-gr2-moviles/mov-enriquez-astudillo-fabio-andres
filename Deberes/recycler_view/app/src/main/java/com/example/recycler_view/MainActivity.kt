e68b4b2d020246846b24001b6ae7abfd33480750
Fabio <fabio.and1514@gmail.com>
2019-06-19 08:14:13 -0500

    header and footer

--- Deberes/recycler_view/app/src/main/java/com/example/recycler_view/MainActivity.kt
+++ Deberes/recycler_view/app/src/main/java/com/example/recycler_view/MainActivity.kt
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.DefaultItemAnimator
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import com.example.myapplication.AdaptadorPersona
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -12,17 +13,37 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        this.supportActionBar?.hide()
 
         val list = ArrayList<Persona>();
-
         list.add(Persona("FB","12"))
         list.add(Persona("DN","34"))
+        list.add(Persona("DN","34"))
+        list.add(Persona("DN","34"))
+        list.add(Persona("DN","34"))
+        list.add(Persona("DN","34"))
+        list.add(Persona("DN","34"))
+        list.add(Persona("DN","34"))
+        list.add(Persona("DN","34"))
 
-        val adaptador = AdaptadorPersona(list, this, rv_main)
+        iniciarRecylerView(list, this, rv_main)
 
-        rv_main.adapter = adaptador
-        rv_main.itemAnimator = DefaultItemAnimator()
-        rv_main.layoutManager = LinearLayoutManager(this)
+    }
 
+    fun iniciarRecylerView(
+        lista: List<Persona>,
+        actividad: MainActivity,
+        recycler_view: RecyclerView
+    ) {
+        val adaptadorPersona = AdaptadorPersona(
+            lista,
+            actividad,
+            recycler_view
+        )
+        recycler_view.adapter = adaptadorPersona
+        recycler_view.itemAnimator = DefaultItemAnimator()
+        recycler_view.layoutManager = LinearLayoutManager(actividad)
+
+        adaptadorPersona.notifyDataSetChanged()
     }
 }
